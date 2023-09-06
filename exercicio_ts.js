aabd6d9408b8576c88ddcf037fb6d5ba0d665bb6
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2023-09-06 01:13:40 -0300

    first commit

---
+++ exercicio_ts.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicacao = (a, b) => a * b;
+const saudacao = (nome) => `Olá, ${nome}!`;
+console.log(multiplicacao(5, 2));
+console.log(saudacao('Gabriel'));
---
+++ exercicio_ts.ts
@@ -0,0 +1,5 @@
+const multiplicacao = (a: number, b: number): number => a * b;
+const saudacao = (nome: string): string => `Olá, ${nome}!`;
+
+console.log(multiplicacao(5, 2))
+console.log(saudacao('Gabriel'))
